8afdad8418e49087956efa3f774e52c790e70860
anderson <Prey.anderson2@gmail.com>
2021-09-01 16:22:22 -0300

    Implementação da RecyclerView

--- app/src/main/java/br/com/bootcampkotlin/appcontentprovider/MainActivity.kt
+++ app/src/main/java/br/com/bootcampkotlin/appcontentprovider/MainActivity.kt
@@ -1,11 +1,47 @@
 package br.com.bootcampkotlin.appcontentprovider
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.bootcampkotlin.appcontentprovider.dataBase.NoteProvider.Companion.URI_NOTES
+import br.com.bootcampkotlin.appcontentprovider.dataBase.NotesDataBaseHelper.Companion.TITLE_NOTES
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+
+    lateinit var noteRecyclerView: RecyclerView
+    lateinit var noteAdd: FloatingActionButton
+
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteAdd = findViewById(R.id.ab_add)
+        noteAdd.setOnClickListener {}
+
+        adapter = NotesAdapter()
+        adapter.setHasStableIds(true)
+
+        noteRecyclerView = findViewById(R.id.rv_notes)
+        noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        noteRecyclerView.adapter = adapter
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+            CursorLoader(this, URI_NOTES, null, null, null, TITLE_NOTES)
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null){}
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
